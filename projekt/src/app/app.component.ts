6141f5168f865a30f52c9db8b7f282e5b5b48d00
Puzsi19hun <125877529+Puzsi19hun@users.noreply.github.com>
2025-01-27 13:08:16 +0100

    login page

--- projekt/src/app/app.component.ts
+++ projekt/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar_logged/navbar.component';
+import { NavbarGuestComponent } from './navbar-guest/navbar-guest.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, NavbarGuestComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  logged_in = false
   title = 'projekt';
 }
---
+++ projekt/src/app/navbar-guest/navbar-guest.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar-guest',
+  imports: [RouterLink],
+  templateUrl: './navbar-guest.component.html',
+  styleUrl: './navbar-guest.component.css'
+})
+export class NavbarGuestComponent {
+
+}
