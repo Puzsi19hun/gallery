beee25189af17eff2a6006d5f745cc92b27ba1bc
Puzsi19hun <puzsi19@gmail.com>
2025-02-06 20:51:08 +0100

    regisztráció basic

--- projekt/src/app/register/register.component.ts
+++ projekt/src/app/register/register.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { DataserviceService } from '../dataservice.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  constructor(private dataservice: DataserviceService, private http: HttpClient) { }
 
+  onSubmit(name: string, email: string, pass1: string, pass2: string) {
+    if (pass1 == pass2) {
+      let url = "https://nagypeti.moriczcloud.hu/PixelArtSpotlight/register"
+
+      let headerss = new HttpHeaders();
+      headerss.set('X-Requested-With', 'XMLHttpRequest')
+      headerss.set('Content-Type', 'application/json')
+      let formData: FormData = new FormData();
+      formData.append('name', name);
+      formData.append('email', email);
+      formData.append('password', pass1);
+
+      this.http.post(url, formData, { headers: headerss, observe: "response" }).subscribe(
+        data => {
+          console.log(data)
+          document.getElementById('hiba')!.innerHTML = "Registration successful!"
+          document.getElementById('hiba')!.style.color = "green"
+        },
+        error => {
+          document.getElementById('hiba')!.innerHTML = error
+        }
+      )
+
+    }
+    else {
+      document.getElementById('hiba')!.innerHTML = "The passwords do not match!"
+    }
+  }
 }
